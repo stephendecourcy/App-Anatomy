647dc0f76ec4eb510efa62e92f6ee240a7675e55
Stephen de Courcy <stephen.decourcy@feedhenry.com>
2012-11-30 10:39:15 +0000

    This is the second commit

--- shared/config.js
+++ shared/config.js
@@ -7,7 +7,7 @@ var config = {
       'Check out <a href="http://docs.feedhenry.com/wiki/App_Anatomy" target="_blank">this link</a> for full documentation on using packages.'
     ],
     tab2: [
-      'The "default" package contains the core of the app. It is represented by the directory /client/default.',
+      'I am Joe',
       'The main start file for this app is index.html - it\'s full path is /client/default/index.html.',
       'To begin exploring this app, start by looking at the index.html file.',
       'The main JavaScript for this app is in init.js - here we initialise the contents of the tabs, and set up scrolling.',
@@ -16,12 +16,12 @@ var config = {
       "Nam congue fringilla odio non pulvinar. In id neque leo, vitae vehicula massa. Cras varius orci posuere massa congue quis vulputate libero varius. Quisque rutrum tellus vel nisi interdum id congue mauris mattis. Proin suscipit elit id magna blandit dignissim facilisis tellus pharetra. Aenean justo quam, egestas in ornare ac, gravida in leo. Donec viverra ante vel lectus consectetur vitae consectetur justo euismod. Etiam ligula sem, tristique sed sollicitudin vitae, pharetra vel sapien. Nam non sem dignissim velit fermentum tincidunt sit amet sit amet lectus. Integer lobortis luctus tortor et tempor. Aliquam id molestie libero. Nunc erat risus, convallis quis feugiat at, lobortis non orci. Sed a felis ante, ut rhoncus massa. Suspendisse potenti."
     ],
     tab3: [
-      'The extra packages contain an override prefs.js which positions the tabs at different areas on screen.',
+      'I am Pual',
       'The iOS and Android packages contain different UI themes - which consist of some different css and different images.',
       'Try adding each of these packages to the Studio Preview under the Configuration accordion item and see the differences after refreshing this preview.'
     ],
     tab4: [
-      'The content for the tabs in this app are configured in /shared/config.js',
+      'I am Dan',
       'When the app starts up, the latest config is read from the server, and used.',
       'If the config can\'t be retrieved, a fallback of the last known configuration is used.',
       'If there was no last configuration, the app defaults to using the config bundled with the app.',
